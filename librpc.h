f8166bf3fe3a8004cea80191b5327e71c13c9ac1
a5dey <a5dey@uwaterloo.ca>
2014-03-15 04:29:23 -0400

    Client functions

--- librpc.h
+++ librpc.h
@@ -19,5 +19,5 @@ int rpcRegister(char *name, int *argTypes, skeleton f);
 int rpcExecute(void);
 int rpcCall(char *name, int *argTypes, void **args);
 int rpcTerminate (void);
-int rpcCacheCall(char * name, int * argTypes, void ** args)
+int rpcCacheCall(char * name, int * argTypes, void ** args);
 
