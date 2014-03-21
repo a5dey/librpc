e1e2d4e0a6cd7af81da58af8d9bd630beea5d907
Shreya Agrawal <s8agrawal@scslt100.cs.uwaterloo.ca>
2014-03-21 14:04:37 -0400

    removed unnecessary comments

--- libserver.cpp
+++ libserver.cpp
@@ -99,16 +99,10 @@ int handleExecute(exeMsg *msg, int _sockfd, struct thread_data *arg)
         reason = FUNC_NOT_FOUND;
     if (reason < 0)
     {
-        //sucFailMsg *sentMsg = new sucFailMsg;
-        //sentMsg->type = EXECUTE_FAILURE;
-        //sentMsg->reason = reason;
         byteMsgSent = createSucFailMsg(EXECUTE_FAILURE, reason);
     }
     else
     {
-        //exeMsg *sentMsg = new exeMsg;
-        //sentMsg = msg;
-        //sentMsg->type = EXECUTE_SUCCESS;
         byteMsgSent = createExeSucMsg(EXECUTE_SUCCESS, msg->name, msg->argTypes, msg->args);
         
     }
@@ -135,10 +129,6 @@ void* handleIncomingConn(void *threadArg)
             handleExecute((exeMsg*)rcvdMsg, _sockfd, arg);
             close(_sockfd);
             pthread_exit((void *)1);
-        //case TERMINATE:
-        //    handleTerminate(_sockfd);
-        //    close(_sockfd);
-        //    pthread_exit((void *)1);
         default:
             retMsg = createTermMsg(MESSAGE_INVALID);
     }
